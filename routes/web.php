d8ceab54751cb73783f088adaa02580f76e4eb40
Marius Herlea <mariusherlea@gmail.com>
2021-04-27 17:39:27 +0300

    add item to order

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Account;
+use App\Models\Item;
+use App\Models\Order;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/createAccount/{name}/{email}', function ($name,$email){
+$account=new Account(['name'=>$name, 'email'=>$email]);
+$account->save();
+});
+
+
+Route::get('/createOrder', function (){
+    $account=Account::findOrFail(1);
+    $account->orders()->save(new Order());
+});
+
+Route::get('/createItem/{name}/{price}/{stock}', function ($name,$price,$stock){
+    $item=new Item(['name'=>$name, 'price'=>$price, 'stock'=>$stock]);
+    $item->save();
+});
+
+
+Route::get('/addItemToOrder', function (){
+    $order=Order::find(1);
+    $item=Item::find(1);
+    $order->items()->save($item);
 });
